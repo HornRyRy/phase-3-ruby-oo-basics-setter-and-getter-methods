5687f7dcbb7e261e63828a1a6e1d9ca33dad9858
Ryan Horn <rj.horn@gmail.com>
2023-01-11 19:57:24 -0700

    assignment done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    
+    def name
+        @name
+    end
 
+    def name= (value)
+        @name = value
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed= (value)
+        @breed = value
+    end
 end
